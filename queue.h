d686bf76e6a0964e822b270c2e98ad1810bc3ccd
gentner.ja@husky.neu.edu <gentner.ja@husky.neu.edu>
2020-01-26 14:02:14 -0500

    Make into idiomatic CwC

--- queue.h
+++ queue.h
@@ -6,34 +6,19 @@
 * Author: lesliexie
 * THIS REMAINS INCOMPLETE
 */
-#ifndef QUEUE_H
-#define QUEUE_H
 
 class Queue {
 public:
 
 	//Pushes an Object onto the Queue
-	void push(Object*);
+	virtual void push(Object*);
 	
-	void push(char*);
-
 	//Returns the Object at end of the Queue
 	Object* pop();
 
-	char* pop();
-
 	//Returns true if this Queue is empty, false otherwise
-	bool isEmpty();
+	virtual bool isEmpty();
 
 	//Deletes this Queue from memory
-	void destroy();
-
-
-private:
-
+	virtual ~Queue();
 };
-
-//Creates a Queue
-Queue* create();
-
-#endif
